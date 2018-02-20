3fae0a3f76de5358987c87f18827711f8f6ba127
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-20 04:23:46 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+  if block_given?
   i = 0
 
   while i < array.length
@@ -6,4 +7,6 @@ def hello_t(array)
     i = i + 1
   end
   array
+else
+  puts "Hey! No block was given!"
 end
